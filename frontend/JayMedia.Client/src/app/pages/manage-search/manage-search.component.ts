c16060178af71b9059842839a423e0b139d82826
Joy-Ese <ihamajoyeseosa@gmail.com>
2025-02-25 21:43:40 +0000

    generate pages

---
+++ frontend/JayMedia.Client/src/app/pages/manage-search/manage-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-search',
+  imports: [],
+  templateUrl: './manage-search.component.html',
+  styleUrl: './manage-search.component.css'
+})
+export class ManageSearchComponent {
+
+}
