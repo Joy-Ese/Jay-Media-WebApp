c55f9f6478dbb23ead4dbe757ea527dc0cf8bd70
Joy-Ese <ihamajoyeseosa@gmail.com>
2025-03-02 16:41:08 +0000

    - add Nlog for logging errors and information for easy debugging - nlog writes log files to C Drive

--- backend/JayMedia.Api/Program.cs
+++ backend/JayMedia.Api/Program.cs
@@ -1,65 +1,86 @@
 global using Microsoft.EntityFrameworkCore;
 global using JayMedia.Api.Data;
+using NLog;
+using NLog.Web;
 
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try 
+{
+  var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+  // Add services to the container.
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+  // NLog: Setup NLog for Dependency injection
+  builder.Logging.ClearProviders();
+  builder.Host.UseNLog();
 
+  // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+  // builder.Services.AddOpenApi();
+  builder.Services.AddControllers();
 
-// Add DBContext For EntityFramework
-builder.Services.AddDbContext<DataContext>(options =>
+  // Add DBContext For EntityFramework
+  builder.Services.AddDbContext<DataContext>(options =>
   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+  // Services are injected here to be available app wide
 
-// Services are injected here to be available app wide
 
+  builder.Services.AddEndpointsApiExplorer();
+  builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+  var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-if (app.Environment.IsDevelopment())
+  // Configure the HTTP request pipeline.
+  // if (app.Environment.IsDevelopment())
+  // {
+  //     app.MapOpenApi();
+  // }
+  if (app.Environment.IsDevelopment())
+  {
+    // Migrate latest database changes during startup
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
+  }
+
+  app.UseSwagger();
+  app.UseSwaggerUI();
+
+  app.UseHttpsRedirection();
+
+  var summaries = new[]
+  {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  };
+
+  app.MapGet("/weatherforecast", () =>
+  {
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+      new WeatherForecast
+      (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+    return forecast;
+  }).WithName("GetWeatherForecast");
+
+  app.Run();
+}
+catch (Exception ex)
 {
-  // Migrate latest database changes during startup
-  using var scope = app.Services.CreateScope();
-  var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-  dbContext.Database.Migrate();
+  // NLog: catch setup errors
+  logger.Error(ex, "Stopped program because of exception");
+  throw;
+}
+finally 
+{
+  // Ensure to flush and stop internal timers/ threads before application-exit (Avoid segmentation fault on Linux)
+  NLog.LogManager.Shutdown();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast =  Enumerable.Range(1, 5).Select(index =>
-    new WeatherForecast
-    (
-      DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-      Random.Shared.Next(-20, 55),
-      summaries[Random.Shared.Next(summaries.Length)]
-    ))
-    .ToArray();
-  return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
